8fb8b75a80efb77039399d5957cd426eed08e018
iridion <antinoridavide@outlook.it>
2020-02-12 18:47:35 +0100

    Ah heck, here we go again.

--- fattoriale.js
+++ fattoriale.js
@@ -1,3 +1,13 @@
-/**
-* Scrivere qui il codice della funzione fattoriale(n)
-**/
+/*
+** Scrivete la funzione ricorsiva di calcolo del fattoriale di un numero n
+** e descrivete lo Stack di chiamate ricorsive
+*/
+
+/* 
+** Marco oserebbe affermare che ció risulti maggiorment leggibile (Vero, boss?). 
+** Io d'altro canto definirei le seguenti righe diabolicamente concise.
+** I disegnini la prossima volta >:/
+*/
+
+const factorial = n => n === 0 ? 1 : n * factorial(n - 1)
+console.log(factorial(5))
